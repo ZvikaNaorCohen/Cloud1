a2a38720b1d54db409ef9eb91a4a84ea4f116d8b
Zvi Naor Cohen <zvinaorco@mta.ac.il>
2023-03-11 13:00:40 +0200

    Page 16: Done GET functions, didn't implement what's happening if invalid input.

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import json
+import collections
 
 from flask import Flask, request, jsonify, make_response
+from collections import OrderedDict
 import requests
-import urllib.parse
-
+import json
 app = Flask(__name__)
 
 # Python list
-dishes_list = []
+dishes_list = [{}]
 
 def check_if_name_exists_in_list(name):
     if name in dishes_list:
@@ -61,10 +61,13 @@ def add_dish():
     content_type = request.headers.get('Content-Type')
     if content_type != 'application/json':
         return make_response(jsonify(0), 415)
+
     data = request.get_json()
     response = check_for_errors(data)
+    # "None" means no errors in the previous checks.
     if response is not None:
         return response
+
     dish_name = data['name']
     dishes_list.append(dish_name)
     index = dishes_list.index(dish_name)
@@ -74,15 +77,56 @@ def add_dish():
 
 @app.get('/dishes')
 def get_json_all_dishes():
-    # Scan the array, and for every name in it - get details from ninjasAPI and add it to a json file.
-    # At the end, return the json file.
     combined_json = {}
     for index, dish_name in enumerate(dishes_list):
         api_url = 'https://api.api-ninjas.com/v1/nutrition?query={}'.format(dish_name)
         response = requests.get(api_url, headers={'X-Api-Key': 'j5GLOwZ/nqeLvuK8bUn00w==0p7X3UH2sBwzMYva'})
         json_dict = response.json()
-        combined_json[str(index)] = json_dict[0]
+        if json_dict:
+            combined_json[str(index)] = show_only_requested_json_keys(json_dict[0])
     return json.dumps(combined_json, indent=4)
+
+@app.get('/dishes/<id_or_name>')
+def get_specific_dish(id_or_name):
+    if "0" <= str(id_or_name[0]) <= "9":
+        return get_dish_by_id(id_or_name)
+    else:
+        return get_dish_by_name(id_or_name)
+
+
+def get_dish_by_id(dish_id):
+    dish_id = int(dish_id)
+    if dish_id == 0 or dish_id >= len(dishes_list):
+        return
+    else:
+        dictforjson = get_dictionary_for_json(dish_id)
+        return json.dumps(dictforjson, indent=4)
+
+def get_dish_by_name(name):
+    try:
+        index_of_dish = dishes_list.index(name)
+        dictforjson = get_dictionary_for_json(index_of_dish)
+        return json.dumps(dictforjson, indent=4)
+    except ValueError:
+        return
+
+def get_dictionary_for_json(dish_index):
+    api_url = 'https://api.api-ninjas.com/v1/nutrition?query={}'.format(dishes_list[dish_index])
+    response = requests.get(api_url, headers={'X-Api-Key': 'j5GLOwZ/nqeLvuK8bUn00w==0p7X3UH2sBwzMYva'})
+    json_dict = response.json()
+    return show_only_requested_json_keys(json_dict[0])
+
+def show_only_requested_json_keys(original_dict):
+    new_dict = OrderedDict()
+    index_of_dish = dishes_list.index(original_dict["name"])
+    new_dict["name"] = original_dict["name"]
+    new_dict["ID"] = index_of_dish
+    new_dict["cal"] = original_dict["calories"]
+    new_dict["size"] = original_dict["serving_size_g"]
+    new_dict["sodium"] = original_dict["sodium_mg"]
+    new_dict["sugar"] = original_dict["sugar_g"]
+
+    return new_dict
 
 
 app.run(host="localhost", port="8496", debug=True)
